fbbaa4c13500c560b5b141092d28f28983a43744
JRCQD <ryan>
2025-05-27 14:12:55 +0100

    small change to build

--- add_thp_env_var.sh
+++ add_thp_env_var.sh
@@ -1,2 +1,2 @@
-MALLOC_CONF="thp:always,metadata_thp:always" cargo build --release
-mv target/release/one_brc .
+MALLOC_CONF="thp:always,metadata_thp:always" cargo build --release --target x86_64-unknown-linux-gnu
+mv target/x86_64-unknown-linux-gnu/release/one_brc .
